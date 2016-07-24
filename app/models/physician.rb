6f113502a1241cef2754438746558b1f236fc1de
abettergrey <abettergrey@gmail.com>
2016-07-24 17:17:38 -0400

    Fixed problem with array return

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -7,8 +7,10 @@ class Physician < ActiveRecord::Base
 
   validates :physician_phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, 
     message: "bad format" }
-
+	@resluts = []
   def result_analytes
-  	@resluts = self.specimens.resluts.where("reported_at >= 2014-01-01 AND reported_at < 2014-04-01").analytes.analyte_name
+  	self.specimens.resluts.where("reported_at >= 2014-01-01 AND reported_at < 2014-04-01").analytes.each do |item|
+  		@resluts.push(item.analyte_name);
+  	end
   end
 end
